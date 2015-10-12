4f1167e0425de9deaef79bbdf0b19517c60509b5
Shawn Anderson <shawn42@gmail.com>
2015-10-12 18:04:16 -0400

    updated for token based auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,24 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  # protect_from_forgery with: :exception
-  # protect_from_forgery :except => :create 
-  skip_before_filter :verify_signed_out_user
+  protect_from_forgery with: :null_session
+
+  protected
+
+  def authenticate_user_from_token!
+    authenticated = authenticate_with_http_token do |user_token, options|
+        user_email = options[:user_email].presence
+        user       = user_email && User.find_by_email(user_email)
+
+        if user && Devise.secure_compare(user.authentication_token, user_token)
+          sign_in user, store: false
+        else
+          render json: 'Invalid authorization.'
+        end
+      end
+
+    if !authenticated
+      render json: 'No authorization provided.'
+    end
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,28 +1,16 @@
 class SessionsController < Devise::SessionsController
   def create
     respond_to do |format|
-      format.html { super }
       format.json do
-        # self.resource = warden.authenticate!(auth_options)
-        # http://stackoverflow.com/q/10717667
-        user = current_user
-
-        authenticate_with_http_token do |token, options|
-          user_email = options[:email].presence
-          user       = user_email && User.find_for_authentication(email: user_email)
-
-          if user && Devise.secure_compare(user.authentication_token, token)
-            sign_in user, store: false
-          end
-
-        end
-
+        self.resource = warden.authenticate!(auth_options)
+        sign_in(resource_name, resource)
         data = {
-          token: user.authentication_token,
-          email: user.email
+          token: self.resource.authentication_token,
+          email: self.resource.email
         }
         render json: data, status: 201
       end
     end
   end
 end
+# now add before_filter :authenticate_user_from_token! to protected routes
